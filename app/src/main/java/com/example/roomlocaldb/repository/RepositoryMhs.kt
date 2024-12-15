585c82f7c9d0f2ce53d82a41354d96d6f3ff525b
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-12-15 11:42:54 +0700

    mendefinisikan fungsi repository untuk pengelolaan data mahasiswa. mendukung operasi membaca semua data, membaca data berdasarkan nim, menghapus, dan memperbarui data mahasiswa.

--- app/src/main/java/com/example/roomlocaldb/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/roomlocaldb/repository/RepositoryMhs.kt
@@ -1,8 +1,15 @@
 package com.example.roomlocaldb.repository
 
 import com.example.roomlocaldb.data.entity.Mahasiswa
+import java.util.concurrent.Flow
 
 //Jika di Dao ada insert di repository juga harus ada insert
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+
+    fun getAllMhs() : Flow<List<Mahasiswa>>
+    fun getMhs(nim: String) : Flow<Mahasiswa>
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
 }
