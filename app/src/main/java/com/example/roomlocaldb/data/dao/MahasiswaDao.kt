ce25241f0eef133db0b95ac3a2b94e8f63666959
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-12-15 11:27:04 +0700

    hapus fungsi untuk menghapus data mahasiswa dari room database. menggunakan suspend function untuk operasi asinkron.

--- app/src/main/java/com/example/roomlocaldb/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/roomlocaldb/data/dao/MahasiswaDao.kt
@@ -1,6 +1,7 @@
 package com.example.roomlocaldb.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Update
 import com.example.roomlocaldb.data.entity.Mahasiswa
@@ -9,6 +10,13 @@ import com.example.roomlocaldb.data.entity.Mahasiswa
 @Dao
 interface MahasiswaDao {
 
+
+
+
+
+
+    @Delete
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
 
     @Update
     suspend fun updateMahasiswa (mahasiswa: Mahasiswa)
