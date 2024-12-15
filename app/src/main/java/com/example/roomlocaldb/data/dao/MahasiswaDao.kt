fa86eb55fe8b0951883ddd41b01d23f3f83bfce9
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-12-15 11:22:19 +0700

    update fungsi untuk memperbarui data mahasiswa menggunakan suspend function di room database. menambahkan parameter mahasiswa untuk mempermudah pengelolaan data.

--- app/src/main/java/com/example/roomlocaldb/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/roomlocaldb/data/dao/MahasiswaDao.kt
@@ -2,11 +2,16 @@ package com.example.roomlocaldb.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Update
 import com.example.roomlocaldb.data.entity.Mahasiswa
 
 
 @Dao
 interface MahasiswaDao {
+
+
+    @Update
+    suspend fun updateMahasiswa (mahasiswa: Mahasiswa)
 
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
