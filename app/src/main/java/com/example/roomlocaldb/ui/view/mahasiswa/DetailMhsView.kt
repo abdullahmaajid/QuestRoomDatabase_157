c083345925dd5667e004c93fe80ae06f9873869a
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-12-17 06:22:34 +0700

    update event ke state

--- app/src/main/java/com/example/roomlocaldb/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/roomlocaldb/ui/view/mahasiswa/DetailMhsView.kt
@@ -54,7 +54,7 @@ fun DetailMhsView(
     viewModel: DetailMhsViewModel = viewModel(factory = PenyediaViewModel.Factory),
     onBack: () -> Unit = { },
     onEditClick: (String) -> Unit ={ },
-    onDeleteClick: () -> Unit)
+    onDeleteClick: () -> Unit ={ } )
 {
     Scaffold (topBar = {
         CustomTopAppBar(
@@ -65,7 +65,7 @@ fun DetailMhsView(
     },
         floatingActionButton = {
             FloatingActionButton(
-                onClick = {onEditClick(viewModel.detailUiEvent.value.detailUiEvent.nim)},
+                onClick = {onEditClick(viewModel.detailUiState.value.detailUiEvent.nim)},
                 shape = MaterialTheme.shapes.medium,
                 modifier = Modifier.padding(16.dp)
             ) {
@@ -75,7 +75,7 @@ fun DetailMhsView(
             }
         }){
             innerPadding ->
-        val detailUiState by viewModel.detailUiEvent.collectAsState()
+        val detailUiState by viewModel.detailUiState.collectAsState()
         BodyDetailMhs(
             modifier = Modifier.padding(innerPadding),
             detailUiState = detailUiState,
