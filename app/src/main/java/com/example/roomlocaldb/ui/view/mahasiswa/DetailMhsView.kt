dad92c3414a588bf4ce3d036bb9cebc62a9d4320
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-12-16 15:27:11 +0700

    menambahkan komponen untuk menampilkan detail mahasiswa dengan format judul dan isinya. Komponen ini menggunakan `Column` untuk menata teks dengan gaya font yang jelas dan terstruktur.

--- app/src/main/java/com/example/roomlocaldb/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/roomlocaldb/ui/view/mahasiswa/DetailMhsView.kt
@@ -14,6 +14,28 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
+@Composable
+fun ComponentDetailMhs(
+    modifier: Modifier =Modifier,
+    judul: String,
+    isinya: String,
+){
+    Column(modifier =modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start)
+    {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
 
 @Composable
 private fun DeleteConfirmationDialog(
