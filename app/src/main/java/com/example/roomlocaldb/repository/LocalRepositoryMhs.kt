721a5a4fbf1dd56b9c12b2085713240e4d6112d1
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-12-17 07:59:38 +0700

    melengkapi code LocalRepositoryMhs.kt

--- app/src/main/java/com/example/roomlocaldb/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/roomlocaldb/repository/LocalRepositoryMhs.kt
@@ -2,28 +2,28 @@ package com.example.roomlocaldb.repository
 
 import com.example.roomlocaldb.data.dao.MahasiswaDao
 import com.example.roomlocaldb.data.entity.Mahasiswa
-import java.util.concurrent.Flow
+import kotlinx.coroutines.flow.Flow
 
-//DI repository buat kan satu interface dan class untuk satu entitas
 class LocalRepositoryMhs (
     private val mahasiswaDao: MahasiswaDao
-) : RepositoryMhs {
+): RepositoryMhs{
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
     }
+
     override fun getAllMhs(): Flow<List<Mahasiswa>> {
         return mahasiswaDao.getAllMahasiswa()
     }
 
-    override fun getMhs(nim: String): Flow<Mahasiswa> {
+    override fun getMhs(nim: String): Flow<Mahasiswa> {  //mengambil data mahasiswa berdasarkan nim
         return mahasiswaDao.getMahasiswa(nim)
     }
 
-    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) { //menghapus data mahasiswa
         mahasiswaDao.deleteMahasiswa(mahasiswa)
     }
 
-    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) { //memperbarui data mahasiswa dalam database
         mahasiswaDao.updateMahasiswa(mahasiswa)
     }
 }
